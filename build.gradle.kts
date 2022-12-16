c17467730901cdd13ed7eb21586f18d1de196271
arrazyfathan <arrazy.rabbani266@gmail.com>
2022-12-16 11:03:48 +0700

    setup firebase crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ buildscript {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0-beta01")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.28-alpha")
         classpath("com.google.gms:google-services:4.3.13")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
     }
 }
 
