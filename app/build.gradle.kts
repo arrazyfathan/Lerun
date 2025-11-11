ae7a53d572703a0ae922a18726a34e9c781038f3
arrazyfathan <arrazy.rabbani266@gmail.com>
2025-11-11 14:46:13 +0700

    chore: update target sdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,13 +16,13 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 36
     namespace = "com.arrazyfathan.lerun"
 
     defaultConfig {
         applicationId = "com.arrazyfathan.lerun"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.2"
 
