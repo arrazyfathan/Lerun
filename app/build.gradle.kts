9a38ff6ce77f2655bdcb3e5a593a97fbdcdcffbe
arrazyfathan <arrazy.rabbani266@gmail.com>
2026-04-30 14:41:45 +0700

    chore: Update version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
